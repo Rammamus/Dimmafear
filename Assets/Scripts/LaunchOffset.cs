21152cb991009e056f2c4a46cd416341678c7b4a
Rammamus <adrian.folin@elev.ga.lbs.se>
2023-02-27 19:41:32 +0100

    hextech rocketbelt works don't touchn my code irt will break

--- Assets/Scripts/LaunchOffset.cs
+++ Assets/Scripts/LaunchOffset.cs
@@ -7,19 +7,23 @@ public class LaunchOffset : MonoBehaviour
     public Transform tm;
     public Player player;
 
+    public bool canRotate;
+
     // Start is called before the first frame update
     void Start()
     {
         tm = GetComponent<Transform>();
-
+        canRotate = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 targetDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-        float angle = (Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg);
-        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
+        if (canRotate)
+        {
+            Vector3 targetDir = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            float angle = (Mathf.Atan2(targetDir.y, targetDir.x) * Mathf.Rad2Deg);
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        }
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,9 +10,14 @@ public class Player : MonoBehaviour
     public bool invincible = false;
     public Transform flashPoint;
     public SummonerSpell SumSpell;
+    public LaunchOffset launchOffset;
     Rigidbody2D rb;
     Vector2 movement;
 
+    public bool canMove = true;
+
+    public bool hexbelt = false;
+    public float hexbeltTimerDontQuestionIt = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -24,27 +29,50 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        if (canMove)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+        }
 
         if (Input.GetKey(KeyCode.F) && SumSpell.GetComponent<SummonerSpell>().canUseSum == true)
         {
             if (SumSpell.usingFlash)
             {
                 transform.position = flashPoint.position;
-                print("using flash");
+                SumSpell.canUseSum = false;
             }
             else if (SumSpell.usingGhost)
             {
                 playerSpeed = 8;
                 invincible = true;
                 float timer = 0;
-                print("using ghost");
+                SumSpell.canUseSum = false;
             }
             else if (SumSpell.usingHexBelt)
             {
+                hexbelt = true;
             }
-            SumSpell.canUseSum = false;
+        }
+        if (hexbelt == true)
+        {
+            launchOffset.canRotate = false;
+            canMove = false;
+            hexbeltTimerDontQuestionIt += Time.deltaTime;
+            float distance;
+            distance = Vector2.Distance(transform.position, flashPoint.transform.position);
+            Vector2 direction = flashPoint.transform.position - transform.position;
+
+            transform.position = Vector2.MoveTowards(this.transform.position, flashPoint.transform.position, 20 * Time.deltaTime);
+            if (hexbeltTimerDontQuestionIt > 0.15f)
+            {
+                hexbelt = false;
+                SumSpell.canUseSum = false;
+                launchOffset.canRotate = true;
+                canMove = true;
+                hexbeltTimerDontQuestionIt = 0;
+                //now make object spawn :D
+            }
         }
     }
     private void FixedUpdate()
